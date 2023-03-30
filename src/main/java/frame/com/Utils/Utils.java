50a2d24fc48b2fc3329538b50220fcd025c16d18
danutsendroiu <danut.sendroiu@yahoo.com>
2023-03-30 20:24:24 +0300

    Update Utils

--- src/main/java/frame/com/Utils/Utils.java
+++ src/main/java/frame/com/Utils/Utils.java
@@ -1,4 +1,21 @@
 package frame.com.Utils;
 
+import java.util.Random;
+
 public class Utils {
+    Random value = new Random();
+
+    public String randomString() {
+
+        char[] array = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j'};
+        String name = "";
+        for (int i = 0; i < 10; i++) {
+            name = name + array[value.nextInt(10)];
+        }
+        return name;
+    }
+
+    public int randomInt() {
+        return value.nextInt(30);
+    }
 }
